f079dc03ea6e68615b081e5aa05ad2a87bb66af9
Aldric Giacomoni <trevoke@gmail.com>
2014-06-05 23:04:55 -0400

    Oof, too much logic for a single test.

--- lib/such_doge_very_wow/parser.rb
+++ lib/such_doge_very_wow/parser.rb
@@ -2,10 +2,55 @@ module SuchDogeVeryWow
   class Parser
     def initialize(dson)
       @dson = dson
+      @current_node_index = 0
+      @nodes = @dson.split(' ')
+      @parsed_hash = {}
     end
 
     def call
-      {}
+      while current_node_index < nodes.length
+        case nodes[current_node_index]
+        when 'such' then new_key
+        when 'is' then new_value
+        when 'wow' then add_key_value_pair
+        end
+        increment_node_index
+      end
+      parsed_hash
     end
+
+    private
+
+    attr_accessor :dson, :current_node_index, :parsed_hash, :nodes
+
+    def increment_node_index
+      @current_node_index += 1
+    end
+
+    def new_key
+      increment_node_index
+      next_key = nodes[current_node_index]
+      return if next_key == 'wow'
+      @key = clean next_key
+    end
+
+    def new_value
+      next_key = nodes[current_node_index + 1]
+      key_type = next_key == 'so' ? 'array' : 'value'
+      if key_type == 'value'
+        increment_node_index
+        @value = clean nodes[current_node_index]
+      end
+    end
+
+    def clean(doge_bit)
+      doge_bit.scan(/"(.*)"/)[0][0]
+    end
+
+    def add_key_value_pair
+      parsed_hash[@key] = @value
+      @key = @value = nil
+    end
+
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,6 +6,11 @@ describe SuchDogeVeryWow do
     expect(parse(doge)).to eq({})
   end
 
+  it 'parses the sample foo/bar object' do
+    doge = 'such "foo" is "bar" wow'
+    expect(parse(doge)).to eq({'foo' => 'bar'})
+  end
+
   def parse(dson)
     SuchDogeVeryWow.parse(dson)
   end
